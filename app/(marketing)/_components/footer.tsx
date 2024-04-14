e4fe0dd818bdd1737619652c8eb37c62d736e0a9
PowerDev <luizotavionunesfelix@gmail.com>
2024-04-14 11:15:50 -0300

    2: marketing page

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+
+export const Footer = () => {
+  return (
+    <div className="dark:bg-[#1F1F1F] flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+        <Button variant="ghost" size="sm">
+          Política de Privacidade
+        </Button>
+        <Button variant="ghost" size="sm">
+          Termos e Condições
+        </Button>
+      </div>
+    </div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,8 +32,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
